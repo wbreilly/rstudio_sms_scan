e9a248e85fa1d121c4d6434793641590fb19ec36
Walter Reilly <wbreilly@ucdavis.edu>
2017-11-03 19:47:33 -0700

    got some prelim plots and analysis going

--- rsa_group_stats.R
+++ rsa_group_stats.R
@@ -1,5 +1,73 @@
 # RSA sum stats
+library(tidyverse)
+library(afex)
+
+
+rsa =read.table("/Users/wbr/walter/fmri/sms_scan_analyses/rsa_singletrial/singletrial_4_rsatoolbox/RSAmeans_SVSS_11_3_17.txt", header = TRUE, stringsAsFactors = TRUE)
+rsa.sumstats = rsa %>% group_by(roi, condition) %>% summarise(mean = mean(similarity), sd = sd(similarity))
+
+rsa.fixed = rsa %>% filter(condition != "random")
+rsafixed.sumstats = rsa.fixed %>% group_by(roi, condition) %>% summarise(mean = mean(similarity), sd = sd(similarity))
+
+all.rois = ggplot(data = rsa, aes(x = condition, y = similarity)) + geom_boxplot() + facet_wrap( ~roi, ncol=6)
+all.rois
+
+
+# the rois
+PM_rois = c('near-phc-ant-L', 'near-phc-ant-R', 'lh-ANG', 'rh-ANG', 'lh-PCC', 'rh-PCC',	'lh-Prec',	'rh-Prec', 'lh-RSC', 'rh-RSC')
+AT_rois = c('near-prc-L',	'near-prc-R', 	'lh-TPole', 	'rh-TPole'	)
+HIPP_rois = c('near-hipp-body-L',	'near-hipp-body-R',	'near-hipp-head-L',	'near-hipp-head-R')
+PM_HIPP_rois = c('near-hipp-body-L',	'near-hipp-body-R',	'near-hipp-head-L',	'near-hipp-head-R', 
+                 'near-phc-ant-L', 'near-phc-ant-R', 'lh-ANG', 'rh-ANG', 'lh-PCC', 'rh-PCC',	'lh-Prec',	'rh-Prec', 'lh-RSC', 'rh-RSC')
+vmpfc = c('near-VMPFC') 
+
+# make networks factor
+# first make logical vectors for member regions
+rsa$PM = rsa$roi %in% PM_rois
+rsa$AT = rsa$roi %in% AT_rois
+rsa$HIPP = rsa$roi %in% HIPP_rois
+rsa$PM_HIPP = rsa$roi %in% PM_HIPP_rois
+
+# now assign group label in single column
+rsa$roi_group = ifelse(rsa$PM == TRUE, "PM", ifelse(rsa$AT == TRUE, "AT", ifelse(rsa$HIPP == TRUE,"HIPP", "Other" )))
+ # delete the logical vectors
+rsa[,5:8] = NULL
+# make group a factor
+rsa$roi_group = as.factor(rsa$roi_group)
+
+########################
+# PMAT AT and HIPP only
+rsa.PMAT = rsa %>% filter(roi_group != "Other")
+
+# plot by cluster
+clusters.rois = ggplot(data = rsa.PMAT, aes(x = factor(condition), y = similarity)) + geom_point(aes(colour = factor(sub))) + geom_boxplot() + facet_wrap( ~roi_group)
+clusters.rois
+
+##########################
+# stats
+m1 = aov_ez("sub", "similarity", rsa.PMAT, within = c("condition", "roi_group"))
+m1
+# not surprising
+
+# looking for main effect so only intact and scrambled
+rsa.main = rsa %>% filter(condition != "random")
+# split df's by roi_group
+rsa.PM = rsa.main %>% filter(roi_group == "PM")
+rsa.AT = rsa.main %>% filter(roi_group == "AT")
+rsa.HIPP = rsa.main %>% filter(roi_group == "HIPP")
+# pairwise ttests
+# sample code
+pairwise.t.test(rsa.AT$similarity,rsa.PM$condition, p.adjust.method = "bonferroni")
+
+#  anova.. but this is doing same as paired ttest 
+m1.PM = aov_ez("sub", "similarity", rsa.PM, within = c("condition"))
+m1.PM
+m1.AT = aov_ez("sub", "similarity", rsa.AT, within = c("condition"))
+m1.AT
+m1.HIPP = aov_ez("sub", "similarity", rsa.HIPP, within = c("condition"))
+m1.HIPP
 
 
 
-rsa =read.table("~/drive/grad_school/DML_WBR/Sequences_Exp3/sms_scan_drive/toy_data/RSAmeans_SVSS_11_2_17.txt", header = TRUE, stringsAsFactors = TRUE)
+
+
